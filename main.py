760e1676e875ce12a2ff7602f33dfc2d894191df
lamoanky <liuoliver08@gmail.com>
2025-11-26 00:36:47 -0700

    added display steps and delete step buttons

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import pygame
 import math
 # pygame setup
@@ -23,16 +22,48 @@ firstClick = True
 
 
 class Button():
-    def __init__(self, x, y, image, scale):
+    def __init__(self, x, y, image, scale, s):
         width = image.get_width()
         height = image.get_height()
         self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
+        self.s = s
+
     def draw(self):
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.s == "display":
+                printSteps()
+                pygame.time.delay(200)
+            if pygame.mouse.get_pressed()[0] == 1 and self.s == "delete":
+                global firstClick
+                if len(coordinates) > 1:
+                    coordinates.pop()
+                    steps.pop()
+                    field.fill((255, 255, 255))
+                    field.blit(pygame.transform.rotate(unrotatedField, 90), (0,0))
+                    pygame.draw.circle(field, (255,0,0), coordinates[0], 6)
+                    for i in range(1, len(coordinates)):
+                        drawArrow(field, coordinates[i-1], coordinates[i])
+                    pygame.time.delay(200)
+                    print("deleted last step :(")
+                elif len(coordinates) == 1:
+                    coordinates.pop()
+                    field.fill((255, 255, 255))
+                    field.blit(pygame.transform.rotate(unrotatedField, 90), (0,0))
+                    firstClick = True
+                    pygame.time.delay(200)
+                    print("deleted starting point :(")
+                elif len(coordinates) == 0:
+                    print("no steps to delete :(")
+                    firstClick = True
+                    pygame.time.delay(200)
+
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
-displayButton = Button(850, 100, pygame.image.load("images/displaySteps.png"), 0.75)
+displayButton = Button(850, 100, pygame.image.load("images/displaySteps.png"), 0.75, "display")
+deleteButton = Button(850, 400, pygame.image.load("images/deleteStep.png"), 0.5, "delete")
 
 
 def drawArrow(surface, startPos, endPos):
@@ -72,19 +103,30 @@ def printMovement():
     print(step)
     steps.append(step)
 
+def printSteps():
+    print("Movement Steps:")
+    counter = 1
+    for step in steps:
+        print(f"Step #{counter}: {step}")
+        counter += 1
 
-
+def checkBoundary(pos):
+    if pos[0] < 660 and pos[0] > 170 and pos[1] > 160 and pos[1] < 640:
+        return True
+    return False
+#170, 660
 running = True
 
 while running:
     # poll for events
-    
+    pos = pygame.mouse.get_pos()
+
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         
-        if event.type == pygame.MOUSEBUTTONUP and not firstClick:
+        if event.type == pygame.MOUSEBUTTONUP and not firstClick and checkBoundary(pos):
             coordinates.append(pygame.mouse.get_pos())
 
             start = coordinates[-2]
@@ -92,7 +134,7 @@ while running:
             drawArrow(field, start, end)
             printMovement()
             
-        if event.type == pygame.MOUSEBUTTONUP and firstClick:
+        if event.type == pygame.MOUSEBUTTONUP and firstClick and checkBoundary(pos):
             firstClick = False
             coordinates.append(pygame.mouse.get_pos())
             pygame.draw.circle(field, (255,0,0), coordinates[0], 6)
@@ -100,6 +142,7 @@ while running:
     # RENDER YOUR
     screen.blit(field, (0, 0))
     displayButton.draw()
+    deleteButton.draw()
     
 
     # flip() the display to put your work on screen
@@ -107,7 +150,4 @@ while running:
 
     clock.tick(60)  # limits FPS to 60
 
-pygame.quit()
-
-
-        
+pygame.quit()        
